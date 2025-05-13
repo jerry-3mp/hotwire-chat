57911e061a0e8c46ba482088de65a9d8df428167
Jerry Tsai <jerry.3mp@gmail.com>
2025-05-13 17:18:50 +0800

    use broadcasts to handle change syncing between different clients

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,16 +1,7 @@
  import { Controller } from "@hotwired/stimulus"
 
  export default class extends Controller {
-     connect() {
-         alert("Controller connected!")
-
-         console.log("Controller name:", this.identifier)
-         console.log("Element:", this.element)
-     }
-
      reset(event) {
-         console.log("Reset method called", event)
-         console.log("Form element:", this.element)
          this.element.reset()
      }
  }
